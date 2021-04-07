c4b5f6ad4c9e8304cbe3375552130bc93746ed13
Christine Jeon <csjeon28@gmail.com>
2021-04-07 00:01:00 -0400

    commit tree 8859384496d5b1aa9d3486710f1ef0eb81bd2447

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 2;
+const num2 = 31;
+let multiply;
+multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random;
+random = Math.random() * 10 + 1;
+random = Number.parseInt(random, 10);
+
+//random = Number.parseInt((Math.random() * 10) + 1, 10);
+
+const num3 = 18
+const num4 = 7
+let mod;
+mod = num3 % num4;
+
+let max;
+max = Math.max(8, 11, 14, 18, 20);
